c8ba1f525289d43a2de73c8ded65699acb403b0d
abdullahabdy <emailtoabdullahmunir@gmail.com>
2023-11-12 10:52:54 +0500

    KualiteeProject

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,11 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
- // reporter: 'cypress mochawesome-reporter',
+ // reporter: 'cypress-mochawesome-reporter',
   e2e: {
     setupNodeEvents(on, config) {
      // require('cypress-mochawesome-reporter/plugin')(on);
-      // implement node event listeners here
+   //  require('cypress-mochawesome-reporter/plugin')(on);
     },
   },
 });
